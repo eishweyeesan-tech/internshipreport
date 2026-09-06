6fa07b061e3c8569634875b5d13990a86e9c213d
eishweyeesan-tech <eishweyeesan@gmail.com>
2026-09-06 14:45:11 +0630

    complete

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 require_once 'config/db.php';
 require_once __DIR__ . '/includes/academic_year_helper.php';
 
--- supervisor/supervisor-student-search-api.php
+++ supervisor/supervisor-student-search-api.php
@@ -12,7 +12,9 @@
  * Response: { results: [...], has_more: bool, total: int }
  */
 
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 header('Content-Type: application/json; charset=utf-8');
 
